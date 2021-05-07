f8b14f874e5014fe16761a1cafc3b9fd23fe5604
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2021-05-07 23:44:09 +0900

    add new page component js

---
+++ src/js/components/HotOsint.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+export default class HotOsint extends React.Component {
+  
+  constructor(props) {
+    super();
+  }
+  
+  render() {
+    const { name, slug, url } = this.props;
+    return (
+        <>
+        
+        </>
+    //   <tr>
+    //       <td><a href={slug}>{name}</a></td>
+    //       <td><span>{url}</span></td>
+    //   </tr>
+    );
+  }
+
+}
+
