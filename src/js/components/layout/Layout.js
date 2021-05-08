ddd79d5e949748c6ee1ce5a2c498e69d2eea7fad
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2021-05-08 23:45:00 +0900

    erase unnecessary lines

--- src/js/components/layout/Layout.js
+++ src/js/components/layout/Layout.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Container, Col, Row } from "react-bootstrap"
 import { connect } from "react-redux";
-import { Link, withRouter } from "react-router-dom";
+import { withRouter } from "react-router-dom";
 
 import Footer from "./Footer";
 import Nav from "./Nav";
@@ -10,16 +10,13 @@ import Nav from "./Nav";
     return {
       user: store.userReducer.user,
       userFetched: store.userReducer.fetched,
-      tweets: store.tweetsReducer.tweets,
-      tweetsFetching: store.tweetsReducer.fetching
+      osints: store.osintsReducer.osints,
+      osintsFetching: store.osintsReducer.fetching
     };
   })  
 class Layout extends React.Component {
   render() {
     const { location } = this.props;
-    const containerStyle = {
-      marginTop: "60px"
-    };
     return (
       <div>
         <Nav location={location} />
@@ -29,43 +26,7 @@ class Layout extends React.Component {
               {this.props.children}
             </div>
           </Row>
-
-          
-      {/* <Row>
-        <Col>.col</Col>
-      </Row>
-      <Row>
-        <Col>.col</Col>
-        <Col>.col</Col>
-        <Col>.col</Col>
-        <Col>.col</Col>
-      </Row>
-      <Row>
-        <Col xs="3">.col-3</Col>
-        <Col xs="auto">.col-auto - variable width content</Col>
-        <Col xs="3">.col-3</Col>
-      </Row>
-      <Row>
-        <Col xs="6">.col-6</Col>
-        <Col xs="6">.col-6</Col>
-      </Row>
-      <Row>
-        <Col xs="6" sm="4">.col-6 .col-sm-4</Col>
-        <Col xs="6" sm="4">.col-6 .col-sm-4</Col>
-        <Col sm="4">.col-sm-4</Col>
-      </Row>
-      <Row>
-        <Col sm={{ size: 6, order: 2, offset: 1 }}>.col-sm-6 .order-sm-2 .offset-sm-1</Col>
-      </Row>
-      <Row>
-        <Col sm="12" md={{ size: 6, offset: 3 }}>.col-sm-12 .col-md-6 .offset-md-3</Col>
-      </Row>
-      <Row>
-        <Col sm={{ size: 'auto', offset: 1 }}>.col-sm-auto .offset-sm-1</Col>
-        <Col sm={{ size: 'auto', offset: 1 }}>.col-sm-auto .offset-sm-1</Col>
-      </Row> */}
           <Footer />
-        {/* </div> */}
         </Container>
       </div>
     );
