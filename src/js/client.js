b1da4c9fe4a710013f0617e8ca40fd6ab530b3d5
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2021-05-08 14:28:27 +0900

    add HotOsint settigns to render data from backend

--- src/js/client.js
+++ src/js/client.js
@@ -4,14 +4,22 @@ import { Provider } from "react-redux";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import Layout from "./components/Layout/Layout";
+
+import Home from "./components/Home";
+import HotOsint from "./components/HotOsint";
+
 import store from "./store";
+
 
 const app = document.getElementById('app');
 
 ReactDOM.render(
   <Provider store={store}>
     <Router>
-      <Layout />
+      <Layout>
+        <Route path="/" component={Home}></Route>
+        <Route path="/hot_osint" component={HotOsint}></Route>
+      </Layout>
     </Router>
   </Provider>, app);
 
--- src/js/components/HotOsint.js
+++ src/js/components/HotOsint.js
@@ -1,21 +1,65 @@
+import axios from "axios";
 import React from "react";
 
 export default class HotOsint extends React.Component {
   
   constructor(props) {
     super();
+    this.state = {
+      osints: []
+    };
   }
   
+  componentDidMount() {
+    axios.get('http://localhost:8000/api/data')
+      .then(res => { 
+        const osint = res.data;
+        this.setState({
+          osints: [...osint]
+        });
+      });
+  }
+  
+
   render() {
-    const { name, slug, url } = this.props;
+    const { osints } = this.state;
+    const osintsComponet = osints.map((osint) => {
+      // const icon = complete ? "\u2714" : "\u2716";
+      const path = 'detail/' + osint.data_id;
+      let risk = '';  
+      if (osint.malicious_level == 1) {
+        risk = 'Dangerous';
+      } else {
+        risk = 'Safe';
+      }  
+      return (
+      <tr key={osint.data_id} href={osint.data_id} {...osint}>
+        <td><a href={path}>{osint.data_id}</a></td>
+        <td><span><a href={osint.gui_url}>{osint.gui_url}</a></span></td>
+        <td><span>{osint.last_analyzed}</span></td>
+        <td><span>{risk}</span></td>
+      </tr>
+    );
+    });
+    
     return (
-        <>
-        
-        </>
-    //   <tr>
-    //       <td><a href={slug}>{name}</a></td>
-    //       <td><span>{url}</span></td>
-    //   </tr>
+      <div>
+        <h1>OSINT DATA</h1>
+        <table class="table table-striped">
+          <thead>
+            <tr>
+              <th>IP Address</th> 
+              <th>VT GUI</th>
+              <th>Last Time</th>
+              <th>Risk</th>
+            </tr>
+          </thead>
+          <tbody>
+            {osintsComponet}
+          </tbody>
+        </table>
+        {/* <button onClick={this.reloadTodos.bind(this)}>Reload!</button> */}
+      </div>
     );
   }
 
