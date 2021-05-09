ff6d44e5f3764cfcbe26cde1ce396d49a93be1da
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2021-05-09 10:16:43 +0900

    fix and improve about action operation

--- src/js/components/HotOsint.js
+++ src/js/components/HotOsint.js
@@ -2,6 +2,7 @@ import axios from "axios";
 import React from "react";
 import { connect } from "react-redux";
 
+import { fetchOsints } from "../actions/osintActions";
 import { fetchUser } from "../actions/userActions";
 
 
@@ -9,51 +10,90 @@ import { fetchUser } from "../actions/userActions";
   return {
     user: store.userReducer.user,
     userFetched: store.userReducer.fetched,
-    osintss: store.osintsReducer.osints
+    osints: store.osintsReducer.osints,
+    fetching: store.osintsReducer.fetching
   };
 })
 export default class HotOsint extends React.Component {
   
   constructor(props) {
     super();
-    this.state = {
-      osints: []
-    };
+    // this.state = {
+    //   osints: []
+    // };
   }
   
   componentDidMount() {
     this.props.dispatch(fetchUser());
-    axios.get('http://localhost:8000/api/data')
-      .then(res => { 
-        const osint = res.data;
-        this.setState({
-          osints: [...osint]
-        });
-      });
+    // this.props.dispatch(fetchOsints());
+    // axios.get('http://localhost:8000/api/data')
+    //   .then(res => { 
+    //     const osint = res.data;
+    //     this.setState({
+    //       osints: [...osint]
+    //     });
+    //   });
   }
-  
+  fetchOsints() {
+    this.props.dispatch(fetchOsints());
+  }
 
   render() {
-    console.log(this.props.user);
-    const { osints } = this.state;
-    const osintsComponet = osints.map((osint) => {
-      // const icon = complete ? "\u2714" : "\u2716";
-      const path = 'detail/' + osint.data_id;
-      let risk = '';  
-      if (osint.malicious_level == 1) {
-        risk = 'Dangerous';
-      } else {
-        risk = 'Safe';
-      }  
+    const { user, osints, fetching } = this.props;
+    if (fetching === true) {
+      return (<div>fetching...</div>);
+    }
+    if (!osints.length) {
+      console.log(user, osints);
+      return <button onClick={this.fetchOsints.bind(this)}>load osints</button>;
+    }
+  
+    const mappedOsints = osints.map(osint => {
+      // <li key={osint.data_id}>{osint.data_id}</li>
+        const path = 'detail/' + osint.data_id;
+        let risk = '';  
+        if (osint.malicious_level == 1) {
+          risk = 'Dangerous';
+        } else {
+          risk = 'Safe';
+        }  
       return (
-      <tr key={osint.data_id} href={osint.data_id} {...osint}>
-        <td><a href={path}>{osint.data_id}</a></td>
-        <td><span><a href={osint.gui_url}>{osint.gui_url}</a></span></td>
-        <td><span>{osint.last_analyzed}</span></td>
-        <td><span>{risk}</span></td>
-      </tr>
-    );
-    });
+        <tr key={osint.data_id} href={osint.data_id} {...osint}>
+          <td><a href={path}>{osint.data_id}</a></td>
+          <td><span><a href={osint.gui_url}>{osint.gui_url}</a></span></td>
+          <td><span>{osint.last_analyzed}</span></td>
+          <td><span>{risk}</span></td>
+        </tr>
+      );
+  });
+    
+    // const { osints } = this.state;
+    // const osintsComponet = osints.map((osint) => {
+    //   // const icon = complete ? "\u2714" : "\u2716";
+    //   const path = 'detail/' + osint.data_id;
+    //   let risk = '';  
+    //   if (osint.malicious_level == 1) {
+    //     risk = 'Dangerous';
+    //   } else {
+    //     risk = 'Safe';
+    //   }  
+    //   return (
+    //   <tr key={osint.data_id} href={osint.data_id} {...osint}>
+    //     <td><a href={path}>{osint.data_id}</a></td>
+    //     <td><span><a href={osint.gui_url}>{osint.gui_url}</a></span></td>
+    //     <td><span>{osint.last_analyzed}</span></td>
+    //     <td><span>{risk}</span></td>
+    //   </tr>
+    // );
+    // });
+
+    // return (
+    //   <div>
+    //     <h1>{user.name}</h1>
+    //     <ul>{mappedOsints}</ul>
+    //   </div>
+    // );
+
     
     return (
       <div>
@@ -68,10 +108,9 @@ export default class HotOsint extends React.Component {
             </tr>
           </thead>
           <tbody>
-            {osintsComponet}
+            {mappedOsints}
           </tbody>
         </table>
-        {/* <button onClick={this.reloadTodos.bind(this)}>Reload!</button> */}
       </div>
     );
   }
