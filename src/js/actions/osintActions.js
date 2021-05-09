02d740b2020e042481b2b32e48f63a2abfb73c03
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2021-05-09 10:15:53 +0900

    change api server endpoint

--- src/js/actions/osintActions.js
+++ src/js/actions/osintActions.js
@@ -3,9 +3,10 @@ import axios from "axios";
 export function fetchOsints() {
   return function(dispatch) {
     dispatch({type: "FETCH_OSINTS"});
-    axios.get("http://localhost:8000")
+    axios.get("http://localhost:8000/api/data")
       .then((response) => {
         dispatch({type: "FETCH_OSINTS_FULFILLED", payload: response.data})
+        console.log(response.data);
       })
       .catch((err) => {
         dispatch({type: "FETCH_OSINTS_REJECTED", payload: err})
