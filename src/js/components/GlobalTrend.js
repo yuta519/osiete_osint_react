eb229efe74e664857ec7d3967c7d134f5f65903b
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2021-05-10 20:47:25 +0900

    create GlobalTrend.js

---
+++ src/js/components/GlobalTrend.js
@@ -0,0 +1,121 @@
+import React from "react";
+import { Button, Card, CardDeck, Chart } from "react-bootstrap"
+
+
+export default class GlobalTrend extends React.Component {
+  
+  constructor(props) {
+    super();
+  }
+  
+  render() {    
+    return (
+        <>
+          <CardDeck style={{ marginTop: '30px' }}>
+            <Card>
+              <Card.Header as="h5">Featured</Card.Header>
+              <Card.Body>
+                <Card.Title>Special title treatment</Card.Title>
+                <Card.Text className="text-primary" as="h1">
+                  54%
+                </Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+              </Card.Body>
+            </Card>
+
+            <Card>
+              <Card.Header as="h5">Featured</Card.Header>
+              <Card.Body>
+                <Card.Title>Special title treatment</Card.Title>
+                <Card.Text className="text-primary" as="h1">
+                  3.2K
+                </Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+              </Card.Body>
+            </Card>
+
+            <Card>
+              <Card.Header as="h5">Featured</Card.Header>
+              <Card.Body>
+                <Card.Title>Special title treatment</Card.Title>
+                <Card.Text className="text-primary" as="h1">
+                  520
+                </Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+              </Card.Body>
+            </Card>
+
+            <Card>
+              <Card.Header as="h5">Featured</Card.Header>
+              <Card.Body>
+                <Card.Title>Special title treatment</Card.Title>
+                <Card.Text className="text-primary" as="h1">
+                  32%
+                </Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+              </Card.Body>
+            </Card>
+            <Card bg="light" style={{ width: '18rem' }}>
+              <Card.Header>Header</Card.Header>
+              <Card.Body>
+                <Card.Title>Light Card Title</Card.Title>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up the bulk
+                  of the card's content.
+                </Card.Text>
+              </Card.Body>
+            </Card>            
+          </CardDeck>
+
+
+          <CardDeck style={{ marginTop: '30px' }}>
+            <Card>
+              <Card.Img variant="top" src="https://picsum.photos/100/100" />
+              <Card.Body>
+                <Card.Title>Card title</Card.Title>
+                <Card.Text>
+                  This is a wider card with supporting text below as a natural lead-in to
+                  additional content. This card has even longer content than the first to
+                  show that equal height action.
+                </Card.Text>
+              </Card.Body>
+              <Card.Footer>
+                <small className="text-muted">Last updated 3 mins ago</small>
+              </Card.Footer>
+            </Card>
+            <Card>
+              <Card.Img variant="top" src="https://picsum.photos/100/100" />
+              <Card.Body>
+                <Card.Title>Card title</Card.Title>
+                <Card.Text>
+                  This is a wider card with supporting text below as a natural lead-in to
+                  additional content. This card has even longer content than the first to
+                  show that equal height action.
+                </Card.Text>
+              </Card.Body>
+              <Card.Footer>
+                <small className="text-muted">Last updated 3 mins ago</small>
+              </Card.Footer>
+            </Card>
+            <Card>
+              <Card.Img variant="top" src="https://picsum.photos/100/100" />
+              <Card.Body>
+                <Card.Title>Card title</Card.Title>
+                <Card.Text>
+                  This is a wider card with supporting text below as a natural lead-in to
+                  additional content. This card has even longer content than the first to
+                  show that equal height action.
+                </Card.Text>
+              </Card.Body>
+              <Card.Footer>
+                <small className="text-muted">Last updated 3 mins ago</small>
+              </Card.Footer>
+            </Card>
+          </CardDeck>
+
+        </>
+    );
+  }
+
+}
+
