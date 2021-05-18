e66ccac2e6b35fe1b3dc29c296493cc9f3474555
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2021-05-18 23:19:51 +0900

    add table for result

--- src/js/components/OsintDetail.js
+++ src/js/components/OsintDetail.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 import React from "react";
-import { Chart, Component } from "react-google-charts";
+import { Card, Col, Row, Table } from "react-bootstrap"
+import { Chart } from "react-google-charts";
 import { connect } from "react-redux";
 
 import { fetchOsints } from "../actions/osintActions";
@@ -12,32 +13,48 @@ import { fetchUser } from "../actions/userActions";
     user: store.userReducer.user,
     userFetched: store.userReducer.fetched,
     osints: store.osintsReducer.osints,
-    fetching: store.osintsReducer.fetching
-  };
+    fetching: store.osintsReducer.fetching};
 })
 export default class OsintDetail extends React.Component {
   render() {
     const pattern = /^\?ip=(.*)/;
     const osint_id = location.search.match(pattern);
-    console.log(osint_id);
-    return (  
-      <div>
-        <h1>detail : { osint_id[1] }</h1>
-        <Chart width={400} height={120} chartType="Gauge" loader={<div>Loading Chart</div>}
-          data={[
-            ['Label', 'Value'],
-            ['Risk', 20],
-          ]}
-          options={{
-            redFrom: 90,
-            redTo: 100,
-            yellowFrom: 30,
-            yellowTo: 90,
-            minorTicks: 5,
-          }}
-          rootProps={{ 'data-testid': '1' }}
-        />
-      </div>
+    return (
+      <>
+      <h1>About <b>{ osint_id[1] }</b></h1>
+      <Row>
+        <Col xs={3}><Chart width={400} height={120} chartType="Gauge" 
+          loader={<div>Loading Chart</div>}
+          data={[ ['Label', 'Value'], ['Risk', 20], ]}
+          options={{ redFrom: 90, redTo: 100, yellowFrom: 30, yellowTo: 90, 
+            minorTicks: 5 }}
+          rootProps={{ 'data-testid': '1' }}/></Col>
+        <Col xs={9}>
+          <Table striped bordered hover>
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>Result</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>owner</td>
+                <td>null</td>
+              </tr>
+              <tr>
+                <td>gui</td>
+                <td>null</td>
+              </tr>
+              <tr>
+                <td>Malicious Level</td>
+                <td>null</td>
+              </tr>
+            </tbody>
+          </Table>
+        </Col>
+      </Row>
+      </>
     );
   }
 
