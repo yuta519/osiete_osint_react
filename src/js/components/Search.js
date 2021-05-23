189c5a293ab34eb50806441fc5c40c1347c621b4
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2021-05-23 22:15:53 +0900

    add test

--- src/js/components/Search.js
+++ src/js/components/Search.js
@@ -45,6 +45,19 @@ export default class Search extends React.Component {
       });
       console.log(this.state.type);
     }
+    // if (this.state.result) {
+    //   if (this.state.result.data.malicious_level == 1) {
+    //     malicious_level = 'Dangerous';
+    //   } else if (this.state.result.data.malicious_level == 2) {
+    //     malicious_level = 'Suspicious';
+    //   } else if(this.state.result.data.malicious_level == 3) {
+    //     malicious_level = 'Safe';
+    //   } else {
+    //     malicious_level = 'None';
+    //   }
+    // } else {
+    //   malicious_level = null;
+    // }
 
   }
 
@@ -70,6 +83,7 @@ export default class Search extends React.Component {
           <Button style={{ float: 'right' }} onClick={this.handleSubmit}>Search OSINT</Button>
         </Form>
         {this.state.result ? (
+          // { this.state.result.data.malicious_level ?}
           <>
             <h1 style={{ marginTop: '100px'}}>OSIETE Osint Result</h1>
             <Table striped bordered hover>
