0e98aaa2b3f828d4ad988d4fb534c9f5d8f7da5e
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2021-06-05 10:53:23 +0900

    using dangerous_data endpoint in HotOsints

--- src/js/actions/osintActions.js
+++ src/js/actions/osintActions.js
@@ -3,7 +3,8 @@ import axios from "axios";
 export function fetchOsints() {
   return function(dispatch) {
     dispatch({type: "FETCH_OSINTS"});
-    axios.get("http://localhost:8000/api/data")
+    // axios.get("http://localhost:8000/api/data")
+    axios.get("http://localhost:8000/api/dangerous_osint")
       .then((response) => {
         dispatch({type: "FETCH_OSINTS_FULFILLED", payload: response.data})
         console.log(response.data);
@@ -13,6 +14,12 @@ export function fetchOsints() {
       });
   };
 }
+
+
+
+
+
+
 
 export function addTweet(id, text) {
   return {
